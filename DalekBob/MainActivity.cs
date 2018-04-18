9cc04914f98d6c0da0b2356ec8b2abc0637ec209
Nathan Hickling <unfoundbug@gmail.com>
2018-04-18 22:30:05 +0100

    Add solution files and dirs

---
+++ DalekBob/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace DalekBob
+{
+    [Activity(Label = "DalekBob", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Comment to cause changes
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
